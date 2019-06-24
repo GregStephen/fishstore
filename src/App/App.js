8d86e9130a8af32c7bcf660967e212a624175d9d
Greg Stephen <GregAStephen@gmail.com>
2019-06-24 18:17:48 -0500

    set up

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.scss';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <button className="btn btn-danger">FISHSTORE</button>
+      </div>
+    );
+  }
+}
+
+export default App;
