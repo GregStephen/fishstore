d836dedb0adb10fcc5e4bd8793f9d4f2c74fbf1c
Greg Stephen <GregAStephen@gmail.com>
2019-06-25 20:09:35 -0500

    added fishes axios get

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import fishData from '../../helpers/data/fishData';
 import './Inventory.scss';
 
 class Inventory extends React.Component {
+  state = {
+    fishes: [],
+  }
+
+  componentDidMount() {
+    fishData.getFishes()
+      .then(fishes => this.setState({ fishes }))
+      .catch(err => console.error('could not get feesh', err));
+  }
+
   render() {
     return (
       <div className="Inventory col">
